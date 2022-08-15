80dfaccc7b7f4b52561c441a4790873e69a2e4d1
mitokande <benmithat18@gmail.com>
2022-08-15 13:07:50 +0300

    added phpdotenv

--- Test.php
+++ Test.php
@@ -1,17 +1,8 @@
 <?php 
-require_once("ApiConfig.php");
-require_once("Barcode.php");
-$inputJSON = file_get_contents('php://input');
-$input = json_decode($inputJSON, TRUE); //convert JSON
+require_once('vendor/autoload.php');
 
-if($input != null){
-    foreach($input as $order){
-        $image = checkBarcode(getApi(),$order['barcode'])->data->resim;
-        echo $image;
-    }
-    exit();
-}
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-echo 'asd';
-
+echo $_ENV["REGION"];
 ?>

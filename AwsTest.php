d918381af6f421c97af2ad7a180a79f99b843f83
mitokande <benmithat18@gmail.com>
2022-08-16 09:59:47 +0300

    added multiple barcode checking

--- AwsTest.php
+++ AwsTest.php
@@ -2,13 +2,15 @@
 
 require_once("Service/AwsS3Manager.php");
 
-if (isset($_FILES['image'])) {
+// if (isset($_FILES['image'])) {
+//     echo awsUploadPhoto();
+// }
+
+function awsUploadPhoto(){
     $file_name = $_FILES['image']['name'];
     $temp_file_location = $_FILES['image']['tmp_name'];
 
    $service =  new AwsS3Manager();
    $result = $service->Upload($file_name,$temp_file_location);
-   echo json_encode($result);
+   return json_encode($result);
 }
-
-
--- Barcode.php
+++ Barcode.php
@@ -1,6 +1,7 @@
 <?php 
 require_once("ApiConfig.php");
 require_once("Service/AuthManager.php");
+require_once("AwsTest.php");
 
 require_once("Entities/Product.php");
 require_once("Utilities/Result/DataResult.php");
@@ -11,44 +12,75 @@ require_once("Utilities/Result/ErrorDataResult.php");
 require_once("Utilities/Result/SuccessDataResult.php");
 
 
+$barcodeJSON = file_get_contents('php://input');
+$barcode = json_decode($barcodeJSON, TRUE); //convert JSON
 
-if($barcode != null){
+if(isset($_POST['barcode'])){
     $authManager = new AuthManager();
     if(!empty(getallheaders()['token'])){
         $result = $authManager->verifyUserToken(getallheaders()['token']);
     }
     if (isset($result) && $result->success)
     {
-        $user = $result->data;
-        $orderFormId = $user->getUserOrderId();
-        // echo $orderFormId;
-        echo json_encode($orderController->addOrder(getApi(),$orderFormId,$orderInput));
+        echo json_encode(addBarcode(getApi(),$_POST));
         exit();
     }
-    if(!empty($barcode['barcode'])){
-        echo json_encode(checkBarcode(getApi(),$barcode['barcode']));
-    }
+}else if($barcode != null){
+    echo json_encode(checkBarcode(getApi(),$barcode));
 }
 
 
-function addBarcode($jotformAPI,$barcode){
+function addBarcode($jotformAPI,$barcode): DataResult{
 //not yet implemented
+    // $submission = array(
+    //         "5" => $stock['urunAdi'],
+    //         "6" => $stock['barcode'],
+    //         "7" => $stock['stok'],
+    //         "8" => $stock['fiyat'],
+    //         "14" => $image,
+    //         "15" => $user->shopName,
+    //         "13" => $user->email
+    //     );
+    
+    //     // $result = $jotformAPI->createFormSubmission("222202437411037", $submission);
+    //     $result = getApi()->createFormSubmission($stockFormID, $submission);
+    
+    $resim = json_decode(awsUploadPhoto())->data;
+    $submission = array(
+            "3" => $barcode['urunAdi'],
+            "4" => $barcode['barcode'],
+            "8" => $barcode['fiyat'],
+            "9" => $resim
+    );
+    $result = $jotformAPI->createFormSubmission("222211745912045", $submission);
+    return new SuccessDataResult($result,"New Barcode added to the database successfuly");
 }
 
 function checkBarcode($jotformAPI,$barcode): DataResult
 {
     $barcodeTable = $jotformAPI->getFormSubmissions("222211745912045");
-    foreach($barcodeTable as $item){
-        if($item['answers'][4]['answer'] == $barcode){
-            $urun = new Product;
-            $urun->urunAdi = $item['answers'][3]['answer'];
-            $urun->barcode = $item['answers'][4]['answer'];
-            $urun->resim = $item['answers'][9]['answer'];
-            $urun->fiyat = $item['answers'][8]['answer'];
-            return new SuccessDataResult($urun,"Barcode found successfully");
+    if(count($barcode) == 1){
+        $barcode = array($barcode);
+    }
+    $urunler = [];
+    foreach($barcode as $b){
+        foreach($barcodeTable as $item){
+            if($item['status'] == "ACTIVE" && $item['answers'][4]['answer'] == $b['barcode']){
+                $urun = new Product;
+                $urun->urunAdi = $item['answers'][3]['answer'];
+                $urun->barcode = $item['answers'][4]['answer'];
+                $urun->resim = $item['answers'][9]['answer'];
+                $urun->fiyat = $item['answers'][8]['answer'];
+                $urunler[] = $urun;
+            }
         }
     }
+    return new SuccessDataResult($urunler,"Barcode found successfully");
+    exit();
+    
+    
     return new ErrorDataResult(null,"Barcode does not exist in Table");
 }
+
 
 ?>

6b822e6a195e4f9e420000e827d95f0dcf90ec84
Feanor <beratyesbek@gmail.com>
2022-08-17 14:13:49 +0300

    CORS

--- Test.php
+++ Test.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Headers: Token,Origin, X-Requested-With, Content-Type, Accept');
 print_r(getallheaders());
 exit();
 ?>

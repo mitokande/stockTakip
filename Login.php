f22a9bdd3a9b1c24c9938f833d9d787b17c3a3ed
Feanor <beratyesbek@gmail.com>
2022-08-17 13:32:26 +0300

    CORS

--- Login.php
+++ Login.php
@@ -5,9 +5,7 @@ header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 require_once("ApiConfig.php");
 require_once("Service/AuthManager.php");
-$inputJSON = file_get_contents('php://input');
-echo json_decode($inputJSON, TRUE);
-/*
+
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, TRUE); //convert JSON
 
@@ -17,12 +15,12 @@ $authManager = AuthManager::getInstance();
 $result = $authManager->login($username,$password);
 if ($result->success)
 {
-    http_response_code(200);
+    //http_response_code(200);
     echo json_encode($result);
     return;
 }
-http_response_code(400);
+//http_response_code(400);
 echo json_encode($result);
-return;*/
+return;
 
 ?>

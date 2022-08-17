591d8ab64dcd29b6ffa5ab0afde44271d4696914
Feanor <beratyesbek@gmail.com>
2022-08-17 14:09:45 +0300

    CORS

--- Test.php
+++ Test.php
@@ -3,6 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
-
-echo "naber"
+print_r($_REQUEST);
+exit();
 ?>
